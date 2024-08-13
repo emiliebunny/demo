d9691035af2ac099db4bb02f54dac04376662072
emiliebunny <emilie>
2024-08-13 13:50:46 +0000

    try re-submit to force building

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -2,7 +2,7 @@ import { Calendar } from "@/components/Calendar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <div>
         <Calendar />
       </div>
